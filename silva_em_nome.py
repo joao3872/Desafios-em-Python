4b0e6583e352ce95c8b60ff2811670acfa5b6ff1
joao3872 <68516158+joao3872@users.noreply.github.com>
2020-11-28 23:47:32 -0300

    Create silva_em_nome.py

---
+++ silva_em_nome.py
@@ -0,0 +1,9 @@
+# conferi se tem Silva no nome.
+
+nome = input('Digite seu nome completo: ')
+lista = nome.split()
+ver = lista[0]
+confira = ('Silva.title() in ver) or ('Silva'.lower() in ver) or ('SILVA' in nome.upper())
+tudo = (ver[0:][5:] in nome)
+
+print(f'Sexo nome possui Silva ? {confira and tudo}').replace('True', 'Sim').replace('False', 'Não')
