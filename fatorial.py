c8732ee7911e03f08dcb68d351d55fb48f118165
joao3872 <68516158+joao3872@users.noreply.github.com>
2020-11-29 15:50:14 -0300

    Create fatorial.py

---
+++ fatorial.py
@@ -0,0 +1,20 @@
+# Mostra o fatorial do número digitado.
+
+num = int(input('Digite um número, para conferir o seu Fatorial: '))
+fatorial = num
+multiplica = 1
+print(f'{num}! =', end = ' ')
+while fatorial != 0:
+    print(f'{fatorial}', end = ' ')
+    if(num == 1):
+        print(' ')
+    elif(fatorial > 1):
+        print('X', end = ' ')
+    else:
+        print('=', end = ' ')
+    multiplica *= fatorial
+    fatorial -= 1
+if num == 1:
+    print(' ')
+else:
+    print(f'{multiplica}')
