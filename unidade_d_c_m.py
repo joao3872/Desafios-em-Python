0ef92ad384e799ae806cdc336ad0f5db12c11b79
joao3872 <68516158+joao3872@users.noreply.github.com>
2020-11-28 23:34:03 -0300

    Update unidade_d_c_m.py

--- unidade_d_c_m.py
+++ unidade_d_c_m.py
@@ -4,3 +4,4 @@ num = int(input('Digite um número inteiro: '))
 valor = num.zfill(4)
 print(f'O número {num} possui:')
 print(f'Unidade: {valor[3]} \nDezena: {valor[2]} \nCentena: {valor[1]} \nMilhar: {valor[0]}')
+
