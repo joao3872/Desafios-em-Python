9945930508afe86176f2f29c972fa43f489b7327
joao3872 <68516158+joao3872@users.noreply.github.com>
2020-11-29 00:43:47 -0300

    Create analisador_triangulo.py

---
+++ analisador_triangulo.py
@@ -0,0 +1,14 @@
+# conferi, se é possível formar um Triângulo, com os três segmentos, informados.
+
+print('<-' * 14)
+print('Analisador de Triângulo')
+print('->' * 14)
+s1 = float(input('Digite o primeiro segmento: '))
+s2 = float(input('Digite o segundo segmento: '))
+s3 = float(input('Digite o terceiro segmento: '))
+confira = ((s2 - s3) < s1 < s2 + s3) and ((s1 - s3) < s2 < s1 + s3) and ((s1 - s2) < s3 < s1 + s2)
+
+if(confira):
+    print('Os três Segmentos Formam um Triângulo !')
+else:
+    print('Os três Segmentos Não formam um Triângulo.')
