94e27ec84447ddc1869e24ce7da72e963008dc25
joao3872 <68516158+joao3872@users.noreply.github.com>
2020-11-29 15:54:16 -0300

    Create pa2.py

---
+++ pa2.py
@@ -0,0 +1,12 @@
+# Mostra a PA dos números digitados e digitando no limite, pode-se definir o máximo de termos que você quiser saber, de uma PA.
+
+print('Gerador de PA \n ')
+termo = int(input('Digite o Termo: '))
+razao = int(input('Digite a Razão: '))
+calculo = 1
+limite = int(input('Informe o número máximo do limite: '))
+while calculo < limite + 1:
+    decimo = termo + (calculo - 1) * razao
+    print(f'{decimo}', end = ' -> ')
+    calculo += 1
+print('Acabou.')
