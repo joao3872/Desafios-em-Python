bf36f3838a434595353b5c5f09735cdbe0798bc0
joao3872 <68516158+joao3872@users.noreply.github.com>
2020-11-29 15:22:20 -0300

    Update soma_par.py

--- soma_par.py
+++ soma_par.py
@@ -1,4 +1,4 @@
-# De 6 números informados, é feita a Soma somente do números Pares.
+# De 6 números informados, é feita a Soma somente do números Pares. Se não tiver nenhum Par, o resultado, será igual a zero.
 
 soma = 0
 for c in range(1, 7):
