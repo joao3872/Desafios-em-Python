fd2a4c576c94d6b4a11472b2b070667f4cf9a725
joao3872 <68516158+joao3872@users.noreply.github.com>
2020-12-05 23:20:07 -0300

    Update loja.py

--- loja.py
+++ loja.py
@@ -1,17 +1,18 @@
 print('<' * 10, 'Lojas JJ', '>' * 10)
-nome = preco = continua = cont = total = 0
-lista = []
+preco = continua = cont = contM = total = menor = 0
+nome = barato = ''
 while True:
     nome = str(input('Digite o nome do Produto: ')).strip()
-    lista.append(nome)
-    barato = sorted(lista) 
     preco = float(input('Informe o Preço do Produto: R$ '))
     continua = str(input('Deseja continuar ? [Sim / Não] ')).strip().upper()
+    cont += 1
     total += preco
     
     if(preco > 1000):
-        cont += 1
-       
+        contM += 1
+    if(cont == 1 or preco < menor):
+        menor = preco
+        barato = nome
     if(continua == 'SIM'):
         print('---'*10)
     elif(continua == 'NÃO'):
@@ -21,5 +22,5 @@ while True:
         break
     
 print(f'O Total gasto na compra foi de R$ {total :.2f}')
-print(f'A compra possui {cont} Produto(s), que custa(m) mais de R$ 1000,00.')
-print(f'E o Produto mais barato é {barato[0]}')
+print(f'A compra possui {contM} Produto(s), que custa(m) mais de R$ 1000,00.')
+print(f'E o Produto mais barato é {barato} com o custo de R$ {menor :.2f}')
