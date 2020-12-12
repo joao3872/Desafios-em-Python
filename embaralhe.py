f65f91abd9ea05d9ecf0f557587e51191b1dac6f
joao3872 <68516158+joao3872@users.noreply.github.com>
2020-12-11 23:46:52 -0300

    Create embaralhe.py

---
+++ embaralhe.py
@@ -0,0 +1,9 @@
+from random import shuffle
+p = input('Digite o nome do 1° aluno: ')
+s = input('Digite o nome do 2° aluno: ')
+t = input('Digite o nome do 3° aluno: ')
+q = input('Digite o nome do 4° aluno: ')
+lista = [p, s, t, q]
+emb = shuffle(lista)
+
+print(f'A ordem de apresentação dos alunos é: {lista}')
