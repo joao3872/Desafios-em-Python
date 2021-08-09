2528456d98210cc4d7b97ff14699aec99d18e126
joao3872 <68516158+joao3872@users.noreply.github.com>
2021-08-09 13:16:45 -0300

    Update unidade_d_c_m.py

--- unidade_d_c_m.py
+++ unidade_d_c_m.py
@@ -1,4 +1,4 @@
-# Exibe a Unidade, Dezena, Centena é Milhar de um número inteiro. 
+# Exibe a Unidade, Dezena, Centena e Milhar de um número inteiro. 
 
 num = int(input('Digite um número inteiro: '))
 valor = num.zfill(4)
