727b633fadc731853534c321433150958f58e098
João Matheus <joaomatheus11@gmail.com>
2022-01-25 20:06:49 -0300

    Delete desafio113 directory

--- desafio113/principal.py
+++
@@ -1,6 +0,0 @@
-from util import inteiro, real
-
-numInt = inteiro('\nDigite um Número Inteiro: ')
-numFloat = real('\nDigite um Número Real: ')
-
-print(f'\nO Número Inteiro foi: {numInt} e o Número Real foi: {numFloat}')
