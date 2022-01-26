8767061a1f0140cb7687f630d171c1b63a6a7ce7
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:26:06 -0300

    Delete maior_menor_aleatorio.py

--- maior_menor_aleatorio.py
+++
@@ -1,5 +0,0 @@
-from random import sample
-valores = tuple(sample(range(11),5))
-
-print(f'{valores}')
-print(f'O maior valor sorteado foi: {min(valores)} \nO menor valor sorteado foi: {max(valores)}')
