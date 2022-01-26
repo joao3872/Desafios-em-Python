44403ab14e7d1e27299c7455c7511e7f0ff8a837
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:26:15 -0300

    Delete menor_maior_peso.py

--- menor_maior_peso.py
+++
@@ -1,8 +0,0 @@
-# Confere qual peso é o maior, e qual é o menor.
-
-lista = []
-for c in range(1, 6):
-    peso = float(input(f'Informe o Peso da {c}° pessoa: Kg '))
-    lista.append(peso)
-    ordem = sorted(lista)
-print(f'O Menor Peso é: {ordem[0]}Kg \nO Maior Peso é: {ordem[4]}Kg')
