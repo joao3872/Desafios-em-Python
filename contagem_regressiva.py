3b2c27e93c89dbfa99ed9ac6ec2ed762d04d089f
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:18:41 -0300

    Delete contagem_regressiva.py

--- contagem_regressiva.py
+++
@@ -1,8 +0,0 @@
-# contagem de 10 até 0, com For e com um intervalo de 1 segundo, de um para o outro.
-
-from time import sleep
-for c in range(10, -1, -1):
-    print(c)
-    sleep(1)
-print('Feliz Ano Novo !!!')
-
