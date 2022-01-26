ffb2d6706e857d3e2d38867b9c8e3415f0735c88
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:19:16 -0300

    Delete expressao_math_paretenses.py

--- expressao_math_paretenses.py
+++
@@ -1,6 +0,0 @@
-expressao = str(input('Digite uma expressão matemática: \033[36m'))
-
-if(expressao.count('(') == expressao.count(')')):
-    print(f'\033[32m A expressão: {expressao} É Válida !\033[m')
-else:
-    print(f'\033[31m A expressão: {expressao} Não é Válida.\033[m')
