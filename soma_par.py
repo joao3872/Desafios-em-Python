e746dafcd2c39988a2b2a2bf53a32841057f0a9e
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:33:10 -0300

    Delete soma_par.py

--- soma_par.py
+++
@@ -1,8 +0,0 @@
-# De 6 números informados, é feita a Soma somente do números Pares. Se não tiver nenhum Par, o resultado, será igual a zero.
-
-soma = 0
-for c in range(1, 7):
-    num = int(input('Digite um número inteiro: '))
-    if(num % 2 == 0):
-        soma += num
-print(f'A Soma dos números Pares é: {soma}')
