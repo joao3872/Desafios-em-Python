17cc1d14ac9d67458110fc42a04c390e2f616ef0
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:17:02 -0300

    Delete area_com_def.py

--- area_com_def.py
+++
@@ -1,5 +0,0 @@
-def area(largura, comprimento):
-    area = largura * comprimento
-    print(f'\nA área de um terreno {largura :.1f} X {comprimento :.1f} é de {area} m²')
-
-area(largura = float(input('Largura (m): ')), comprimento = float(input('Comprimento (m): ')))
