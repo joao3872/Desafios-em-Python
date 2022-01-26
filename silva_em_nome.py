e1cba42ec7d2ca92c5c7e6122ba7d4f6dfe9e0e6
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:31:26 -0300

    Delete silva_em_nome.py

--- silva_em_nome.py
+++
@@ -1,9 +0,0 @@
-# conferi se tem Silva no nome.
-
-nome = input('Digite seu nome completo: ')
-lista = nome.split()
-ver = lista[0]
-confira = ('Silva.title() in ver) or ('Silva'.lower() in ver) or ('SILVA' in nome.upper())
-tudo = (ver[0:][5:] in nome)
-
-print(f'Seu nome possui Silva ? {confira and tudo}'.replace('True', 'Sim').replace('False', 'Não'))
