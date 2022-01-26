db0afb7f8b94c068148ce24e3b5c2f80d1504115
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:27:02 -0300

    Delete numero_extenso.py

--- numero_extenso.py
+++
@@ -1,6 +0,0 @@
-extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
-numero = int(input('Digite um número de 0 a 20: '))
-
-while numero not in range(0, 21):
-    numero = int(input('Tente novamente... Digite um número de 0 a 20: '))
-print(f'Você digitou o número: {extenso[numero]}')
