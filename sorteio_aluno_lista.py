5d1bea507fa07afa41067b569b08d63ac3cc53c8
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:33:47 -0300

    Delete sorteio_aluno_lista.py

--- sorteio_aluno_lista.py
+++
@@ -1,8 +0,0 @@
-from random import choice
-primeiro = input('Digite o nome do primeiro aluno: ')
-segundo = input('Digite o nome do segundo aluno: ')
-terceiro = input('Digite o none do terceiro aluno: ')
-quarto = input('Digite o nome do quarto aluno: ')
-lista = [primeiro, segundo, terceiro, quarto]
-
-print(f'O aluno sorteado foi {choice(lista)}')
