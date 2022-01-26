ccb378167feb21fa62f0a5dd419ec7afe9370889
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:34:12 -0300

    Delete tabuada_sem_limite.py

--- tabuada_sem_limite.py
+++
@@ -1,7 +0,0 @@
-# Mostra a tabuada dos números informados nos dois input. Não tem o limite de um número X 1 até número X 10.
-
-valor = int(input('Digite um número, para mostrar a Tabuada: '))
-valor2 = int(input('Até onde você, deseja multiplicar? '))
-
-for c in range(1, valor2 + 1):
-    print(f'{valor} X {c :2} = {valor * c}')
