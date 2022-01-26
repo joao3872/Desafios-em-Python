e6b72d6b117362a18db45fdae9ea4a6b397f2bb3
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:26:23 -0300

    Delete medida_parede.py

--- medida_parede.py
+++
@@ -1,6 +0,0 @@
-altura = float(input('Informe a altura em metros de uma parede: '))
-largura = float(input('Informe a largura em metros de uma parede: '))
-area = (altura * largura)
-tinta = (area / 2)
-
-print(f'A área da parede é {area}m² \nE a quantidade de tinta necessária para pintar sua parede é de {tinta}L')
