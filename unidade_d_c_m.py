074dd5ce17d9c07de7c6f45c50d65ac853062895
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:34:52 -0300

    Delete unidade_d_c_m.py

--- unidade_d_c_m.py
+++
@@ -1,7 +0,0 @@
-# Exibe a Unidade, Dezena, Centena e Milhar de um número inteiro. 
-
-num = int(input('Digite um número inteiro: '))
-valor = num.zfill(4)
-print(f'O número {num} possui:')
-print(f'Unidade: {valor[3]} \nDezena: {valor[2]} \nCentena: {valor[1]} \nMilhar: {valor[0]}')
-
