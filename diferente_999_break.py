f7d1738aff8f1b2342319b6d0126efde16f9c346
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:18:50 -0300

    Delete diferente_999_break.py

--- diferente_999_break.py
+++
@@ -1,8 +0,0 @@
-num = cont = soma = 0
-while num != 999:
-    num = int(input('Digite um número (use 999 para parar): '))
-    if num == 999:
-        break
-    cont += 1
-    soma += num
-print(f'A Soma dos {cont} números é {soma}')
