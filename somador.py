17d8e163235d1414de9bd6585fe81c367898a404
João Matheus <joaomatheus11@gmail.com>
2022-01-25 21:33:00 -0300

    Delete somador.py

--- somador.py
+++
@@ -1,8 +0,0 @@
-num = int(input('Quantas Notas Deseja Somar ? '))
-soma = 0
-cont = 0
-for c in range(0, num):
-    cont += 1    
-    valor = float(input(f'Digite o Valor Total da {cont}° Nota: R$ '))
-    soma += valor
-print(f'O valor total é: R$ {soma :.2f}')
